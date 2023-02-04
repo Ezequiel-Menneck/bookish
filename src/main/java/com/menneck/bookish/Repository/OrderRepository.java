c1b45a96c438131f56232959681153ee30781a09
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-02-04 14:52:44 -0300

    fix: change name of class to make more sense

--- src/main/java/com/menneck/bookish/Repository/OrderRepository.java
+++ src/main/java/com/menneck/bookish/Repository/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.menneck.bookish.Repository;
 
-import com.menneck.bookish.Model.ProductOrder;
+import com.menneck.bookish.Model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductOrderRepository extends JpaRepository<ProductOrder, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
